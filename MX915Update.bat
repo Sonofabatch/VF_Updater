fbe8e99be8a28c34858a2adc8e14624384bed063
WisdomWolf <WisdomWolf@gmail.com>
2013-10-16 10:51:31 -0400

    Updated version designed by WisdomWolf

--- MX915Update.bat
+++ MX915Update.bat
@@ -1,8 +1,11 @@
-@echo off
-:again
-cls
+FOR %%A IN (%Date%) DO SET Today=%%A
+SET Now=%Time: =0%
+SET Hours=%Now:~0,2%
+SET Minutes=%Now:~3,2%
+ECHO.%Now% | FIND /I "P" >NUL && SET /A Hours += 12
 echo **********************
 echo Verifone MX915 Update
+echo %Today% %Hours%:%Minutes%
 echo **********************
 echo Choose one of the following...
 echo 1. SHOE SHOW
@@ -14,151 +17,32 @@ echo 6. Altier
 echo 7. Quit
 choice /c:1234567 /N "Enter Selection: "
 if errorlevel 7 goto end
-if errorlevel 6 goto chn6
-if errorlevel 5 goto chn5
-if errorlevel 4 goto chn4
-if errorlevel 3 goto chn3
-if errorlevel 2 goto chn2
-if errorlevel 1 goto chn1
+if errorlevel 6 set store=altier
+if errorlevel 5 set store=burlington
+if errorlevel 4 set store=shoebilee
+if errorlevel 3 set store=encore
+if errorlevel 2 set store=shoedept
+if errorlevel 1 set store=shoeshow
+goto comchoice
+
+:comchoice
+echo Choose one of the following...
+echo 1. COM Port 1 
+echo 2. COM Port 2
+echo 3. Go Back
+echo 4. Quit
+choice /c:1234 /N "Enter Selection: "
+if errorlevel 4 goto end
+if errorlevel 3 goto again
+if errorlevel 2 set comport=2
+if errorlevel 1 set comport=1
+echo Make sure pinpad is powered up
+pause
+
+cd SS_vfQUeverything_20130508A_%store%COM%comport%\vfQueryUpdate
+Echo The time is currently %Hours%:%Minutes%
+Echo Wait 20-40 minutes for update to complete...
+VFQueryUpdate.exe
 goto again
-goto end
-:chn1
-echo Choose one of the following...
-echo 1. COM Port 1 
-echo 2. COM Port 2
-choice /c:12 /N "Enter Selection: "
-if errorlevel 2 goto chn12
-if errorlevel 1 goto chn11
-goto chn1
-goto end
-:chn11
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_shoeshowCOM1\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn12
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_shoeshowCOM2\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn2
-echo Choose one of the following...
-echo 1. COM Port 1 
-echo 2. COM Port 2
-choice /c:12 /N "Enter Selection: "
-if errorlevel 2 goto chn22
-if errorlevel 1 goto chn21
-goto chn2
-goto end
-:chn21
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_shoedeptCOM1\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn22
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_shoedeptCOM2\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn3
-echo Choose one of the following...
-echo 1. COM Port 1 
-echo 2. COM Port 2
-choice /c:12 /N "Enter Selection: "
-if errorlevel 2 goto chn32
-if errorlevel 1 goto chn31
-goto chn3
-goto end
-:chn31
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_encoreCOM1\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn32
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_encoreCOM2\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn4
-echo Choose one of the following...
-echo 1. COM Port 1 
-echo 2. COM Port 2
-choice /c:12 /N "Enter Selection: "
-if errorlevel 2 goto chn42
-if errorlevel 1 goto chn41
-goto chn4
-goto end
-:chn41
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_shoebileeCOM1\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn42
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_shoebileeCOM2\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn5
-echo Choose one of the following...
-echo 1. COM Port 1 
-echo 2. COM Port 2
-choice /c:12 /N "Enter Selection: "
-if errorlevel 2 goto chn52
-if errorlevel 1 goto chn51
-goto chn5
-goto end
-:chn51
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_burlingtonCOM1\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn52
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_burlingtonCOM2\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn6
-echo Choose one of the following...
-echo 1. COM Port 1 
-echo 2. COM Port 2
-choice /c:12 /N "Enter Selection: "
-if errorlevel 2 goto chn62
-if errorlevel 1 goto chn61
-goto chn6
-goto end
-:chn61
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_altierCOM1\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
-:chn62
-echo Make sure pinpad is powered up
-pause
-cd SS_vfQUeverything_20130508A_altierCOM2\vfQueryUpdate
-Echo Wait X minutes for update to complete...
-VFQueryUpdate.exe
-goto end
+
 :end
-rem echo end
